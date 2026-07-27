728c94671ccff9011a856003488a92f9f3cfc6a0
Juraj Kollár <mostypc123@redroselinux.org>
2026-07-27 20:09:28 +0200

    Update mkstage.sh

--- tools/mkstage.sh
+++ tools/mkstage.sh
@@ -3,3 +3,8 @@ make modules_install INSTALL_MOD_PATH=$(pwd)/stage
 make headers_install INSTALL_HDR_PATH=$(pwd)/stage-h
 mkdir $(pwd)/stage/boot
 cp arch/x86/boot/bzImage $(pwd)/stage/boot/vmlinuz-$(make -s kernelrelease)
+echo "version $(make -s kernelrelease)" > stage-h/car
+echo "version $(make -s kernelrelease)" >> stage/car
+echo "exec kernel-switch" >> stage/car
+mkdir -p stage/usr/bin
+cp ../tools/kern_switch.sh stage/usr/bin/kernel-switch
